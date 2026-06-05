5b850c2cb959873ede4b8f105354abde3a285fa3
Qhari Waman <qhariquinteros@gmail.com>
2026-06-05 17:02:34 -0300

    commit inicial

---
+++ ToDo/Program.cs
@@ -0,0 +1,9 @@
+﻿using EspacioTarea;
+List<Tarea> tareasPendientes = new List<Tarea>();
+tareasPendientes.Add(new Tarea(1000, "Servir desayunos", 4));
+tareasPendientes.Add(new Tarea(1001, "Limpiar habitaciones", 5));
+tareasPendientes.Add(new Tarea(1002, "Preparar rellenos", 6));
+tareasPendientes.Add(new Tarea(1003, "Preparar platos principales", 7));
+tareasPendientes.Add(new Tarea(1004, "Preparar menus diarios", 8));
+tareasPendientes.Add(new Tarea(1005, "Preparar postres", 9));
+tareasPendientes.Add(new Tarea(1006, "Hervir pastas", 2));
---
+++ ToDo/Tarea.cs
@@ -0,0 +1,18 @@
+namespace EspacioTarea;
+
+public class Tarea
+{
+    private int tareaID;
+    private int duracion;
+    public int TareaID { get => tareaID; set => tareaID = value; }
+    public string Descripcion { get; set; }
+    public int Duracion { get => duracion; set => duracion = value; } //Validar que esté entre 10 y 100
+
+    //Puedes añadir un constructor y metodos auxiliares si lo consideras necesario
+    public Tarea(int tareaID, string descripcion, int duracion)
+    {
+        TareaID = tareaID;
+        Descripcion = descripcion;
+        Duracion = duracion;
+    }
+}
